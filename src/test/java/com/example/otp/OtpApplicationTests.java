4f1632e1f75d5ddab5a47b07d3fc907bcfd56b39
Jinghua Li <jinghuali@melco-resorts.com>
2022-03-08 18:35:18 +0800

    Remove useless test

--- src/test/java/com/example/otp/OtpApplicationTests.java
+++ src/test/java/com/example/otp/OtpApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.otp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class OtpApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class OtpApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
